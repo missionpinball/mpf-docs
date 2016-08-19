e03570c4fb32a0e0312161facfa309a899c3a0d2
toomanybrians <brian@missionpinball.com>
2016-08-19 15:27:50 -0700

    Create combined conf.py for bootstrap & non-bootstrap environments

--- conf.py
+++ conf.py
@@ -15,7 +15,13 @@
 
 import sys
 import os
-import sphinx_bootstrap_theme
+
+try:
+    import sphinx_bootstrap_theme
+    bootstrap = True
+
+except ImportError:
+    bootstrap = False
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
