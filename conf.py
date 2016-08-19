77778798577656fb8807e311e8b2bedf3575fbe1
toomanybrians <brian@missionpinball.com>
2016-08-19 15:40:39 -0700

    updated conf.py to exclude templates folder with no bootstrap

--- conf.py
+++ conf.py
@@ -43,7 +43,10 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+
+if bootstrap:
+
+    templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
