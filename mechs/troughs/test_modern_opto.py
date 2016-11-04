2eb02ac76216d417a18fa737bc46c345cc75ad36
toomanybrians <brian@missionpinball.com>
2016-11-03 20:35:30 -0700

    added modern style trough with optos docs & tests

---
+++ mechs/troughs/test_modern_opto.py
@@ -0,0 +1,17 @@
+from MpfDocsTestCase import MpfDocsTestCase
+
+
+class TestModernOpto(MpfDocsTestCase):
+
+    rst_target = 'mechs/troughs/modern_opto'
+
+    def test_modern_opto(self):
+        self.assertIn('bd_trough', self.machine.ball_devices)
+
+        self.assertTrue(self.machine.switch_controller.is_active('s_trough1'))
+        self.assertTrue(self.machine.switch_controller.is_active('s_trough2'))
+        self.assertTrue(self.machine.switch_controller.is_active('s_trough3'))
+        self.assertTrue(self.machine.switch_controller.is_active('s_trough4'))
+        self.assertTrue(self.machine.switch_controller.is_active('s_trough5'))
+        self.assertTrue(self.machine.switch_controller.is_active('s_trough6'))
+        self.assertFalse(self.machine.switch_controller.is_active('s_trough_jam'))
