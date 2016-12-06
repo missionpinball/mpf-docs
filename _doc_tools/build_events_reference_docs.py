9b0a1ab2d1b7597e67840dc9022308f0cba22368
Brian Madden <brian@missionpinball.com>
2016-12-06 01:26:22 -0500

    fixed conditional events link

--- _doc_tools/build_events_reference_docs.py
+++ _doc_tools/build_events_reference_docs.py
@@ -46,7 +46,7 @@ understand:
    :maxdepth: 1
 
    overview/index
-   overview/conditional_events
+   overview/conditional
    overview/multiple_things_from_one_event
    overview/priorities
    overview/event_types
