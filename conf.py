321b259acbb32a43d6e064c4283a9d509b153cb9
Brian Madden <brian@fastpinball.com>
2022-08-20 10:51:49 -0700

    updated version message

--- conf.py
+++ conf.py
@@ -240,12 +240,11 @@ def setup(app):
               you'll be on the latest, cutting-edge version of MPF.
 
               However, if you want a more stable version, select the ``v:stable`` version from the
-              lower-left corner of this page, which is for MPF |stable_version|. (You can also
-              read the docs for older MPF versions too.)
+              lower-left corner of this page, which is the most recent version that is not getting new updates.
 
               If you are new to MPF, we have recently rewritten the installation process
               which only applies to this "latest" |version|, so you probably want to stay here
-              because the prior |stable_version| installation process doesn't work on the latest
+              because the prior installation process doesn't work on the latest
               OS and Python versions.
 
         '''
